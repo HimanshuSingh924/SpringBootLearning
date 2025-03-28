cad5266c6ef346e48dbccf60b072b37257861f37
HimanshuSingh924 <72846461+HimanshuSingh924@users.noreply.github.com>
2025-03-28 18:31:06 +0530

    Update Main.java
    Displaying my name

--- src/main/java/org/springlearning/Main.java
+++ src/main/java/org/springlearning/Main.java
@@ -16,5 +16,6 @@ public class Main {
             System.out.println("i = " + i);
         }
         System.out.println("args = " + Arrays.toString(args));
+        System.out.println("Himanshu Singh");
     }
 }
